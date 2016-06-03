f298af2b6551cfc436d9147a6e6b2be697e1009e
Stefan du Fresne <stefan@sdufresne.info>
2016-06-03 17:20:53 +0100

    Let's try this

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ var db = pgp({
   port: 5432
 });
 
-db.query('SELECT session_user, current_user')
-.then(function(result) {
-  console.log(arguments);
+console.log('CT');
+db.query('CREATE TABLE foo (bar int);')
+.then(function() {
+  console.log('AT');
+  return db.query('ALTER TABLE foo OWNER TO session_user');
+})
+.then(function() {
+  console.log('Done!');
   process.exit(0);
+})
+.catch(function(err) {
+  console.log('OH NO', err);
+  process.exit(-1);
 });
